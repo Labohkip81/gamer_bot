1a9ed1339a54021d47e688656e0e1427a3e1420c
labohkip81 <labankiplagat81@gmail.com>
2019-07-03 11:53:46 +0300

    Creted href links using django templating language and learnt on the tests.py file.

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 
-from .views import home_page_view
+from .views import home_page_view,HomePage,AboutPage
 
 
 urlpatterns = [
-    path('', home_page_view, name="home_page")
+    #class based views have to call an as view function.
+    path('', HomePage.as_view(), name='home'),
+    path("about", AboutPage.as_view(), name='about'),
+    
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-
+#This is a fuction-based view in django.
 def home_page_view(request):
     return HttpResponse('Request received 200!')
 # Create your views here.
+
+# This is a class based view in django
+#Class based views should have thir names starting with a capital letter.
+class HomePage(TemplateView):
+    template_name = 'home.html'
+
+class AboutPage(TemplateView):
+    template_name = 'about.html'
